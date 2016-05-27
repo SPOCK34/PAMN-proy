f7dcf7793f73853a0408d721f9075a430d883f25
SPOCK34 <carlospervas@gmail.com>
2016-05-26 22:30:50 -0500

    3

--- JugadorPortero.java
+++ JugadorPortero.java
@@ -8,11 +8,17 @@
 * @version: 26/05/2016/A
 **/
 public class JugadorPortero extends Futbolista{
-	//Atributos
+	/**
+	*Atributos que definen a un portero
+	*dan algunas caracteristicas importantes asi como algunos datos importantes
+	*/
 	protected String tamañomanos,saltos,suerte;
 	protected byte balonesdetenidos;
 	
-	//Costructor
+	/**
+	* Metodo constructor
+	*Metodo constructor sobrecargando  todos los atributos 
+	*/
 	protected JugadorPortero(String tamañomanos, String saltos, String suerte, byte balonesdetenidos, String nombre){
 
 		this.tamañomanos=tamañomanos;
@@ -22,23 +28,37 @@ public class JugadorPortero extends Futbolista{
 		this.nombre= nombre;
 		
 	}
-	//metodos: penal, saque de meta, 
+	/**metodos: 
+	*penal, saque de meta
+	*/ 
+
+	/** metodo que muestra  la accion  de saque de meta
+	*/
 	protected void SaqueMeta(){
 		
 		System.out.println("El portero "+nombre+" hace saque  de meta");
 
 
 	}
+	/**
+	*Metodo  Penal
+	*Muestra un mensaje donde el portero  se prepara para la accion
+	*/
 	protected void Penal(){
 
 		System.out.println("El portero "+nombre+"se prepara para un penal y da "+saltos+" saltos");
 		
 	}
+
+	/**
+	*Este metodo muestra  todos los atributos que un jugador portero puede tener
+	*y ejemplifica el uso de la herencia entre clases 
+	*/
 	protected void MuestraEstadisticas(){
 
 		System.out.println("Datos del Portero: ");
 		System.out.println("Nombre: "+nombre+"\nGenero: "+genero+"\nEstado civil: "+edocivil+"\nEdad: "+edad+"\nPeso: "+peso+"\nAltura: "+altura+"\nNacionalidad: "+nacionalidad);
-		System.out.println("\nApodo: "+apodo+"\nDivisión:"+division+"\nPosición: "+posicion+"\nLesiones: "+Lesiones+"\Experiencia: "+experiencia+);
+		System.out.println("\nApodo: "+apodo+"\nDivisión:"+division+"\nPosición: "+posicion+"\nLesiones: "+lesiones+"\nExperiencia: "+experiencia);
 		System.out.println("\nContrato"+contrato+"\nPosiciones que ocupa: "+posicionesp+"\nSueldo: "+sueldo);
 		System.out.println("\nTitularidad: "+titularidad);
 	}
